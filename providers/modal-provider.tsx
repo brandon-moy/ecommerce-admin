d3312f0d454299b1cada90c7a293e79865f5938f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-09-08 11:04:38 -0700

    Created a modal-provider as preventative measure against modal hydration error

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+};
