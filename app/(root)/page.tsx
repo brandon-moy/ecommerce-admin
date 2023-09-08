005dbfcf5671e4f0a4da90b8dd522c2272330c31
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-09-08 10:19:47 -0700

    Set up Clerk authentitcation routes and components for sign-in/sign-up

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function SetupPage() {
   return (
     <div className="p-4">
       <Button size="default" variant="outline">
