b1a59862e8ab9bfdf85a2df8c9892b3f1ae886d1
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-12 11:41:36 -0700

    Set up select to switch store using shadcn popover and command components

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,11 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 import MainNav from "@/components/Navbar/main-nav";
+import StoreSwitcher from "@/components/Navbar/store-switcher";
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex items-center h-16 px-4">
-        <div>Store switcher</div>
+        <StoreSwitcher />
         <MainNav className="mx-6" />
         <div className="flex items-center ml-auto space-x-4">
           <UserButton afterSignOutUrl="/" />
