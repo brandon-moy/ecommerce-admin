22b3229275da22dca8233f10a2663279674f02d3
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-12 11:43:58 -0700

    Added auth and prisma find to navbar to local stores and pass as prop to store-switcher

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,12 +1,26 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import MainNav from "@/components/Navbar/main-nav";
 import StoreSwitcher from "@/components/Navbar/store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b">
       <div className="flex items-center h-16 px-4">
-        <StoreSwitcher />
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="flex items-center ml-auto space-x-4">
           <UserButton afterSignOutUrl="/" />
