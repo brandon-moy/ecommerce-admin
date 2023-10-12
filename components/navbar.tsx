4f331cfc86eff5779b0b4523fed7bf0bffdde7e2
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-12 11:17:14 -0700

    added pathname, params, and route to use in map function for navlinks with active/hover color

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
+import MainNav from "@/components/main-nav";
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex items-center h-16 px-4">
         <div>Store switcher</div>
-        <div>Routes</div>
+        <MainNav className="mx-6" />
         <div className="flex items-center ml-auto space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
