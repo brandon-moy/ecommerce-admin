eaf0010ca647dcb49e17458d33484bc2e13f92b7
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-16 11:59:37 -0700

    Added styling to reusable heading component

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -4,7 +4,12 @@ interface HeadingProps {
 }
 
 const Heading: React.FC<HeadingProps> = ({ title, description }) => {
-  return <div>Heading</div>;
+  return (
+    <div>
+      <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
+      <p className="text-sm text-muted-foreground">{description}</p>
+    </div>
+  );
 };
 
 export default Heading;
