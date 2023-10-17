f4caf6b04023014f4315cff5f9519f1f59b46e8c
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-16 18:14:16 -0700

    Created GET route to retrieve all billboards for a store

--- app/api/[storedId]/billboards/route.ts
+++ app/api/[storedId]/billboards/route.ts
@@ -51,3 +51,24 @@ export async function POST(
     return new NextResponse("Internal error", { status: 500 });
   }
 }
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const billboard = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARDS_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
