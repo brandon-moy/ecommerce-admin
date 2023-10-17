23f9eb9d05a52917526502c4561dcc3c7cdd5259
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-16 17:43:42 -0700

    Created billboard form as updated version of settings form for billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import Heading from "@/components/ui/heading";
-import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 const BillboardClient = () => {
+  const params = useParams();
+  const router = useRouter();
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -13,7 +17,9 @@ const BillboardClient = () => {
           title="Billboards (0)"
           description="Manage billboards for your store"
         />
-        <Button>
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
           <Plus className="w-4 h-4 mr-2" />
           Add New
         </Button>
