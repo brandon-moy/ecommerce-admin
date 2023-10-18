9da406554fc230d9feb0298baa7e072f731e7ecd
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-18 15:58:16 -0700

    Copied billboards page and components and editing into categoriesd page and components

---
+++ app/(dashboard)/[storeId]/categories/page.tsx
@@ -0,0 +1,35 @@
+import { format } from "date-fns";
+import prismadb from "@/lib/prismadb";
+import CategoryClient from "./components/client";
+import { CategoryColumn } from "./components/columns";
+
+const Categories = async ({ params }: { params: { storeId: string } }) => {
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.createdAt, "MMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex-1 p-8 pt-6 space-y-4">
+        <CategoryClient data={formattedCategories} />
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
