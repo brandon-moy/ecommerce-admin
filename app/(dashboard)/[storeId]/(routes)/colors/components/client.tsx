3efe1a919169ee0dc073202ee7bce340fb9659f9
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-19 13:03:10 -0700

    Copied over files from sizes for colors folder and updated base page and components to use colors instead of sizes

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { ColorColumn, columns } from "./columns";
+import { useParams, useRouter } from "next/navigation";
+import Heading from "@/components/ui/heading";
+import ApiList from "@/components/ui/api-list";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+
+interface ColorsClientProps {
+  data: ColorColumn[];
+}
+
+const ColorsClient: React.FC<ColorsClientProps> = ({ data }) => {
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors (${data.length})`}
+          description="Manage colors for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="w-4 h-4 mr-2" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API calls for colors" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="colorId" />
+    </>
+  );
+};
+
+export default ColorsClient;
