337d9eeed21fdbeb8f1e742845cd527afbe656d6
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-20 15:04:17 -0700

    Removed cell-actions and updated client for orders page

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,43 +1,25 @@
 "use client";
 
-import { Plus } from "lucide-react";
-import { BillboardColumn, columns } from "./columns";
-import { useParams, useRouter } from "next/navigation";
+import { OrderColumn, columns } from "./columns";
 import Heading from "@/components/ui/heading";
-import ApiList from "@/components/ui/api-list";
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface OrderClientProps {
+  data: OrderColumn[];
 }
 
-const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
-  const params = useParams();
-  const router = useRouter();
-
+const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
   return (
     <>
-      <div className="flex items-center justify-between">
-        <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboards for your store"
-        />
-        <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
-        >
-          <Plus className="w-4 h-4 mr-2" />
-          Add New
-        </Button>
-      </div>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
-      <Heading title="API" description="API calls for Billboards" />
-      <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId" />
+      <DataTable searchKey="products" columns={columns} data={data} />
     </>
   );
 };
 
-export default BillboardClient;
+export default OrderClient;
--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import CellAction from "./cell-action";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -35,9 +34,5 @@ export const columns: ColumnDef<OrderColumn>[] = [
   {
     accessorKey: "isPaid",
     header: "Paid",
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => <CellAction data={row.original} />,
   },
 ];
