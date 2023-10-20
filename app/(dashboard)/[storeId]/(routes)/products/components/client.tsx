9971afac442c8fc8efdafc5bbd7ca93038f7b34f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-19 19:41:02 -0700

    Fixed filter issue due to searchKey being set as label instead of name

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -30,7 +30,7 @@ const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
+      <DataTable searchKey="name" columns={columns} data={data} />
       <Heading title="API" description="API calls for Products" />
       <Separator />
       <ApiList entityName="products" entityIdName="productId" />
