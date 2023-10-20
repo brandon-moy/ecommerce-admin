0c84ac217bd5ef5092706e00a953c65a24081870
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-19 19:40:18 -0700

    Corrected wrong display for size that was set as item.category.name and not item.size.name in products/page.tsx

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -26,7 +26,7 @@ const Products = async ({ params }: { params: { storeId: string } }) => {
     isArchived: item.isArchived,
     price: formatter.format(+item.price),
     category: item.category.name,
-    size: item.category.name,
+    size: item.size.name,
     color: item.color.value,
     createdAt: format(item.createdAt, "MMM do, yyyy"),
   }));
